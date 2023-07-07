28eb3de482b809cfe297d4baaf0d5414cdec31b4
Анатолий <80486143+DjHelkern@users.noreply.github.com>
2023-07-07 14:39:33 +0500

    Update test2.sql

--- test2.sql
+++ test2.sql
@@ -1,7 +1,6 @@
 SELECT fr.rfam_acc, fr.rfamseq_acc, fr.seq_start, fr.seq_end, f.type
 FROM full_region fr, rfamseq rf, taxonomy tx, family f
-WHERE
-rf.ncbi_id = tx.ncbi_id
+WHERE rf.ncbi_id = tx.ncbi_id
 AND f.rfam_acc = fr.rfam_acc
 AND fr.rfamseq_acc = rf.rfamseq_acc
 AND tx.tax_string LIKE '%Mammalia%'
